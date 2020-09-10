7153168913346a055fe4ad6f0ace9321891ce9d9
Matt Blum <Matt.blum@live.com>
2020-09-10 11:06:57 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
