494288cb06f3e595bcbb9d91a8661ddb3cfcff6c
Matt Blum <matt.blum@live.com>
2020-09-12 13:41:21 -0500

    Revert "Add ErrorBoundary"
    This reverts commit 3ccb53579af7f75a8bef94665de98914c9f47cf3.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -68,7 +68,7 @@ module.exports = {
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)
-    devtoolModuleFilenameTemplate: (info) =>
+    devtoolModuleFilenameTemplate: info =>
       path
         .relative(paths.appSrc, info.absoluteResourcePath)
         .replace(/\\/g, '/'),
@@ -90,6 +90,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
+      
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -120,6 +121,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
+              
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -147,6 +149,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
+              
               compact: true,
             },
           },
@@ -180,8 +183,6 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
-                        modules: true,
-                        localIdentName: '[name]__[local]__[hash:base64:5]',
                       },
                     },
                     {
